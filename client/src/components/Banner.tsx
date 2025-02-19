68f5448341feb6d7182d1723a22f515001052db7
Tao <ginadrcoder@gmail.com>
2025-02-19 17:40:17 +1100

    create Footer.tsx, modify readme

--- client/src/components/Banner.tsx
+++ client/src/components/Banner.tsx
@@ -5,7 +5,7 @@ import{ useState, useEffect } from 'react'
 
 const messages = [
   "FREE SHIPPING",
-  "FREE SAMPLES WITH ORDERS OVER $100",
+  "SAMPLES WITH ORDERS OVER $100",
   "CHOOSE YOUR FREE GIFT HERE"
 ]
 
@@ -31,21 +31,10 @@ export default function Banner() {
             clipPath:
               'polygon(74.8% 41.9%, 97.2% 73.2%, 100% 34.9%, 92.5% 0.4%, 87.5% 0%, 75% 28.6%, 58.5% 54.6%, 50.1% 56.8%, 46.9% 44%, 48.3% 17.4%, 24.7% 53.9%, 0% 27.9%, 11.9% 74.2%, 24.9% 54.1%, 68.6% 100%, 74.8% 41.9%)',
           }}
-          className="aspect-577/310 w-[36.0625rem] bg-linear-to-r from-[#FEFFAC] to-[#FFB0B0] opacity-30"
+          className="aspect-577/310 w-[36.0625rem] bg-linear-to-r from-[#FCF596] to-[#FFB0B0] opacity-30"
         />
       </div>
-      <div
-        aria-hidden="true"
-        className="absolute top-1/2 left-[max(45rem,calc(50%+8rem))] -z-10 -translate-y-1/2 transform-gpu blur-2xl"
-      >
-        <div
-          style={{
-            clipPath:
-              'polygon(74.8% 41.9%, 97.2% 73.2%, 100% 34.9%, 92.5% 0.4%, 87.5% 0%, 75% 28.6%, 58.5% 54.6%, 50.1% 56.8%, 46.9% 44%, 48.3% 17.4%, 24.7% 53.9%, 0% 27.9%, 11.9% 74.2%, 24.9% 54.1%, 68.6% 100%, 74.8% 41.9%)',
-          }}
-          className="aspect-577/310 w-[36.0625rem] bg-linear-to-r from-[#FEFFAC] to-[#FF8A8A] opacity-30"
-        />
-      </div>
+      
       <p className="text-sm/6 text-gray-900">
         {messages[currentMessageIndex] === "CHOOSE YOUR FREE GIFT HERE" ? (
           <a href="/special-offers" className="whitespace-nowrap">
